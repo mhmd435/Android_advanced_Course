e977deb7ba231762dc415d624c660ab8d76080e0
mhmd aghajani <mhm.aghajani435@gmail.com>
2021-09-03 21:45:25 +0430

    setup Toolbar

--- app/src/main/java/com/example/androidadvancedcourse/HomeFragment/HomeFragment.java
+++ app/src/main/java/com/example/androidadvancedcourse/HomeFragment/HomeFragment.java
@@ -1,22 +1,76 @@
 package com.example.androidadvancedcourse.HomeFragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.androidadvancedcourse.MainActivity;
 import com.example.androidadvancedcourse.R;
+import com.example.androidadvancedcourse.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    FragmentHomeBinding fragmentHomeBinding;
+    MainActivity mainActivity;
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) context;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupToolbar(view);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        fragmentHomeBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return fragmentHomeBinding.getRoot();
+    }
+
+    private void setupToolbar(View view){
+        NavController navController = Navigation.findNavController(view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment)
+                .setOpenableLayout(mainActivity.drawerLayout)
+                .build();
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+
+        NavigationUI.setupWithNavController(toolbar,navController,appBarConfiguration);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getId() == R.id.homeFragment){
+                    toolbar.setNavigationIcon(R.drawable.ic_baseline_sort_24);
+                    toolbar.setTitle("CryptoBs");
+                }
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/androidadvancedcourse/MainActivity.java
+++ app/src/main/java/com/example/androidadvancedcourse/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.androidadvancedcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.PopupMenu;
+
+import com.example.androidadvancedcourse.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
+import me.ibrahimsn.lib.OnItemReselectedListener;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
+
+    NavHostFragment navHostFragment;
+    NavController navController;
+
+    AppBarConfiguration appBarConfiguration;
+
+    public DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        drawerLayout = activityMainBinding.drawerlayout;
+
+        setupNavigationComponent();
     }
+
+    private void setupNavigationComponent() {
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment,R.id.marketFragment,R.id.watchlistfragment)
+                .setOpenableLayout(activityMainBinding.drawerlayout)
+                .build();
+
+        NavigationUI.setupWithNavController(activityMainBinding.navigationView,navController);
+
+
+        setupSmoothBottomMenu();
+    }
+
+    private void setupSmoothBottomMenu() {
+        PopupMenu popupMenu = new PopupMenu(this,null);
+        popupMenu.inflate(R.menu.bottom_navigation_menu);
+        Menu menu = popupMenu.getMenu();
+
+        activityMainBinding.bottomNavigation.setupWithNavController(menu,navController);
+        
+    }
+
+
 }
